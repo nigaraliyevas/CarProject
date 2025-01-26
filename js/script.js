7fa19334e9705616744a3d30435f07716e34d8bc
nigaraliyevas <naliyeva965@gmail.com>
2025-01-26 21:03:57 +0400

    changed

--- js/script.js
+++ js/script.js
@@ -22,6 +22,17 @@ if (window.innerWidth <= 480) {
 }
 
 const Datepicker = window.Datepicker;
+Datepicker.locales.ru = {
+  days: ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"],
+  daysShort: ["Вос", "Пон", "Вто", "Сре", "Чет", "Пят", "Суб"],
+  daysMin: ["Вс", "Пн", "Вт", "Ср", "Чт", "Пт", "Сб"],
+  months: ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"],
+  monthsShort: ["Янв", "Фев", "Мар", "Апр", "Май", "Июн", "Июл", "Авг", "Сен", "Окт", "Ноя", "Дек"],
+  today: "Сегодня",
+  clear: "Очистить",
+  format: "dd.mm.yyyy",
+  weekStart: 1, // Start the week on Monday
+};
 
 const inputs = document.querySelectorAll(".period__input");
 
